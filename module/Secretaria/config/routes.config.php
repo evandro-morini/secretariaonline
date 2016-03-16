ae560cf2c2aed1bf43f7a282e79f286a8ad1b057
Evandro Morini <evandro.morini@gmail.com>
2016-03-15 23:07:20 -0300

    Layout base
    Inserção do layout base para a secretaria online.

--- module/Secretaria/config/routes.config.php
+++ module/Secretaria/config/routes.config.php
@@ -3,7 +3,7 @@ return array(
     'home' => array(
         'type' => 'Literal',
         'options' => array(
-            'route' => '/index',
+            'route' => '/home',
             'defaults' => array(
                 '__NAMESPACE__' => 'Secretaria\Controller',
                 'controller' => 'Home',
